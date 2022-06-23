0d1f7371208204701bcb40efed1c6620bb83e2b7
vfJorge <a19216280@alumnos.uady.mx>
2022-06-23 03:32:30 -0500

    Login y Register parte 1 [F]

--- histolok/src/app/app.module.ts
+++ histolok/src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { PreguntaComponent } from './components/pregunta/pregunta.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    PreguntaComponent
+    PreguntaComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ histolok/src/app/services/login-register.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginRegisterService {
+
+  constructor(private http: HttpClient) { }
+
+  postCrearCuenta(cuenta: any){
+    var _url = 'http://localhost:8000/api/auth/register';
+    let header= new HttpHeaders().set('Content-Type', 'application/json')
+    return this.http.post(_url, cuenta, {
+      headers: header
+    })
+  }
+
+  postIniciarSesion(cuenta: any){
+    var _url = 'http://localhost:8000/api/auth/login';
+    let header= new HttpHeaders().set('Content-Type', 'application/json')
+    return this.http.post(_url, cuenta, {
+      headers: header
+    })
+  }
+
+  guardarToken(bearerToken: any){
+    localStorage.setItem('bearerToken', bearerToken);
+  }
+}
