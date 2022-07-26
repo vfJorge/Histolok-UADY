c1616e551ae076d1638d6a23eed3fc2634a0f227
vfJorge <a19216280@alumnos.uady.mx>
2022-07-26 18:41:29 -0500

    Editar y Eliminar funcionando [F]

--- histolok/src/app/components/images/images.component.ts
+++ histolok/src/app/components/images/images.component.ts
@@ -9,17 +9,72 @@ import { AdminImagesService } from 'src/app/services/admin-images.service';
 export class ImagesComponent implements OnInit {
   public misImagenes: Array<any> = [];
   imagenesURL = "http://127.0.0.1:8000/storage/";
+  archivoCapturado: any;
+  imgTitle: string = "";
+  imgDesc: string = "";
+  imgKeywords: string = "";
+  imgID: any;
 
   constructor(private adminImagesService: AdminImagesService) { }
 
   ngOnInit(): void {
     this.adminImagesService.getMisImagenes().subscribe((resp: any) => {
-      console.log(resp.body);
       this.misImagenes = resp.body;
+      console.log(resp.body)
     }, error => {
       console.log(error);
     })
   }
 
+  eliminarImagen(imagenID: any){
+    this.adminImagesService.delEliminarImagen(imagenID).subscribe((resp: any) => {
+      if(resp.status == 200){
+        alert("Imagen eliminada de manera exitosa");
+        window.location.reload();
+      }
+    }, error => {
+      console.log(error);
+      alert("No se pudo eliminar la imagen, inténtalo de nuevo");
+    })
+  }
   
+  editarImagenVista(imagenID: any, imagenTITLE: any, imagenDESC: any, imagenKEYWORDS: any){
+    this.imgID = imagenID;
+    (<HTMLInputElement>document.getElementById("titulo")).value = imagenTITLE;
+    (<HTMLInputElement>document.getElementById("descripcion")).value = imagenDESC;
+    (<HTMLInputElement>document.getElementById("palabclv")).value = imagenKEYWORDS[0].keyword;
+  }
+
+  enviarEdicion(){
+    const formularioDatos = new FormData();
+    formularioDatos.append('title', this.imgTitle)
+    formularioDatos.append('desc', this.imgDesc)
+    formularioDatos.append('keywords', this.imgKeywords)
+    formularioDatos.append('image', this.archivoCapturado)
+      this.adminImagesService.putEditarImagen(formularioDatos, this.imgID).subscribe((resp: any) => {
+        if(resp.status == 200){
+          alert("Imagen editada de manera exitosa");
+          window.location.reload();
+        }
+      }, error => {
+        console.log(error);
+        alert("No se pudo editar la imagen, inténtalo de nuevo");
+      })
+  }
+
+  getTitle(title: string){
+    this.imgTitle = title;
+  }
+
+  getDesc(desc: string){
+    this.imgDesc = desc;
+  }
+
+  getKeywords(keywords: string){
+    this.imgKeywords = keywords;
+  }
+
+  capturarFile(event: any): any{
+    this.archivoCapturado = event.target.files[0]
+  }
 }
--- histolok/src/app/services/admin-images.service.ts
+++ histolok/src/app/services/admin-images.service.ts
@@ -21,4 +21,18 @@ export class AdminImagesService {
     let header= new HttpHeaders().set('Content-Type', 'application/json').set('Authorization','Bearer '+bearerToken)
     return this.http.get(_url, {headers: header, observe:'response'})
   }
+
+  delEliminarImagen(imagenID: any){
+    var _url = 'http://127.0.0.1:8000/api/fotos/'+imagenID;
+    var bearerToken = localStorage.getItem('bearerToken');
+    let header= new HttpHeaders().set('Accept','application/json').set('Authorization','Bearer '+bearerToken);
+    return this.http.delete(_url, {headers: header, observe:'response'})
+  }
+
+  putEditarImagen(imagenDatos: any, imagenID: any){
+    var _url = 'http://127.0.0.1:8000/api/fotos/'+imagenID+'?_method=PUT';
+    var bearerToken = localStorage.getItem('bearerToken');
+    let header= new HttpHeaders().set('Accept','application/json').set('Authorization','Bearer '+bearerToken);
+    return this.http.post(_url, imagenDatos, {headers: header, observe:'response'})
+  }
 }
