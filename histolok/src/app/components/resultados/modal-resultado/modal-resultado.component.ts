6e61fcb23594137a045c729b7cbbffcce4183afb
vfJorge <a19216280@alumnos.uady.mx>
2023-07-16 19:15:05 -0600

    Finalización de las funcionalidades de la retro [F]

--- histolok/src/app/components/resultados/modal-resultado/modal-resultado.component.ts
+++ histolok/src/app/components/resultados/modal-resultado/modal-resultado.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, Input, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-modal-resultado',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modal-resultado.component.css']
 })
 export class ModalResultadoComponent implements OnInit {
-
-  constructor() { }
+  imagenesURL = "http://127.0.0.1:8000/storage/";
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any, 
+    public dialogRef: MatDialogRef<ModalResultadoComponent>
+  ) { }
 
   ngOnInit(): void {
   }
 
+  cerrar(){
+    this.dialogRef.close()
+  }
 }
